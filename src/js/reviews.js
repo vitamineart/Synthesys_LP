17ebf8535e08fdff1e688b09f56166e596f00c32
Art <vitaminearte@gmail.com>
2021-12-29 18:33:43 +0200

    swiper lazy loading images

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -2,21 +2,23 @@ const reviewsSwiper = new Swiper('.reviews-slider', {
   direction: 'horizontal',
   loop: true,
   speed: 400,
-    effect: 'creative',
-    creativeEffect: {
-        prev: {
-            translate: [0, 0, -1000],
-            opacity: 0
-        },
-        next: {
-            translate: ['100%', 0, 0],
-            opacity: 1
-        },
-    },
+  effect: 'creative',
+  creativeEffect: {
+      prev: {
+          translate: [0, 0, -1000],
+          opacity: 0
+      },
+      next: {
+          translate: ['100%', 0, 0],
+          opacity: 1
+      },
+  },
   navigation: {
     nextEl: '.reviews-next',
     prevEl: '.reviews-prev',
-  }
+  },
+  preloadImages: false,
+  watchSlidesProgress: true
 
 });
 
